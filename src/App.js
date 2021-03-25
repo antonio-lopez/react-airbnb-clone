bd1ef76862a8f486f599836f50e7b9067595fdeb
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-03-24 21:42:06 -0700

    added components directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Home from './Home';
-import Header from './Header';
-import Footer from './Footer';
-import SearchPage from './SearchPage';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SearchPage from './components/SearchPage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
